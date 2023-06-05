26b723b35f738afa5d2932e94b8ab008d1db1b10
[AlexBAlcantara14] <[bancesalcantaralexis@gmail.com]>
2023-06-05 15:36:53 -0500

    first commit

---
+++ src/main/java/idat/edu/pe/demo2_idat/Demo2IdatApplication.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.demo2_idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2IdatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2IdatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idat/edu/pe/demo2_idat/Demo2IdatApplicationTests.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.demo2_idat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2IdatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
